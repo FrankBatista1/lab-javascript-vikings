73d0689140bb11682ebbed6882438e569789a7b1
Frank Batista <fb53901@gmail.com>
2021-08-13 18:17:31 -0400

    Frank Batista and Scott Stossel

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,52 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health,strength){
+    this.health = health,
+    this.strength = strength
+  }
+    attack() {
+        return this.strength;
+    }
+    receivedamage(damage) {
+       this.health -= damage;//how to do that doesnt return anything return ???
+    }
+}
 
 // Viking
-class Viking {}
+class Viking {
+  constructor(health,strength,name){
+  super(health,strength)
+  this.name = name
+}
+ receivedamage(damage) {
+  this.health -= damage
+  if (this.health > 0){
+    return `${this.name} has received ${damage} points of damage`
+  }
+    else{
+      return `${this.name}has died in act of combat`
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  super(health, strength)
+  receivedamage(damage) {
+  this.health -= damage
+  if (this.health > 0){
+    return `${this.health} has received ${damage} points of damage`
+   }
+    else {
+      return `A Saxon has died in act of combat`
+    }
+  }
+
+
 
 // War
 class War {}
